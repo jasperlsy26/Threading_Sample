b909ef4a766153e5d2ab33e9f1d76614fcb27203
Siyang Li <jasperlsy26@gmail.com>
2016-04-04 14:35:19 -0700

    Threading sample excercise
    taught by Evan Olds

---
+++ cpts322hw12 threading/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace cpts322hw12_threading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.urltextbox = new System.Windows.Forms.TextBox();
+            this.download = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.downloadresultbox = new System.Windows.Forms.TextBox();
+            this.button_sorting = new System.Windows.Forms.Button();
+            this.textBox_sortingresult = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.urltextbox);
+            this.groupBox1.Location = new System.Drawing.Point(289, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 51);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "URL";
+            // 
+            // urltextbox
+            // 
+            this.urltextbox.Location = new System.Drawing.Point(6, 19);
+            this.urltextbox.Name = "urltextbox";
+            this.urltextbox.Size = new System.Drawing.Size(301, 20);
+            this.urltextbox.TabIndex = 0;
+            // 
+            // download
+            // 
+            this.download.Location = new System.Drawing.Point(295, 69);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(301, 35);
+            this.download.TabIndex = 1;
+            this.download.Text = "Download string from URL";
+            this.download.UseVisualStyleBackColor = true;
+            this.download.Click += new System.EventHandler(this.download_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.downloadresultbox);
+            this.groupBox2.Location = new System.Drawing.Point(289, 126);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(307, 315);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Download result";
+            // 
+            // downloadresultbox
+            // 
+            this.downloadresultbox.Location = new System.Drawing.Point(18, 30);
+            this.downloadresultbox.Multiline = true;
+            this.downloadresultbox.Name = "downloadresultbox";
+            this.downloadresultbox.ReadOnly = true;
+            this.downloadresultbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.downloadresultbox.Size = new System.Drawing.Size(273, 268);
+            this.downloadresultbox.TabIndex = 0;
+            this.downloadresultbox.TextChanged += new System.EventHandler(this.downloadresultbox_TextChanged);
+            // 
+            // button_sorting
+            // 
+            this.button_sorting.Location = new System.Drawing.Point(12, 12);
+            this.button_sorting.Name = "button_sorting";
+            this.button_sorting.Size = new System.Drawing.Size(271, 51);
+            this.button_sorting.TabIndex = 4;
+            this.button_sorting.Text = "Go sorting";
+            this.button_sorting.UseVisualStyleBackColor = true;
+            this.button_sorting.Click += new System.EventHandler(this.button_sorting_Click);
+            // 
+            // textBox_sortingresult
+            // 
+            this.textBox_sortingresult.Location = new System.Drawing.Point(13, 83);
+            this.textBox_sortingresult.Multiline = true;
+            this.textBox_sortingresult.Name = "textBox_sortingresult";
+            this.textBox_sortingresult.ReadOnly = true;
+            this.textBox_sortingresult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_sortingresult.Size = new System.Drawing.Size(270, 341);
+            this.textBox_sortingresult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 453);
+            this.Controls.Add(this.textBox_sortingresult);
+            this.Controls.Add(this.button_sorting);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.download);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "hw12 threading by Siyang Li 11443579";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox urltextbox;
+        private System.Windows.Forms.Button download;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox downloadresultbox;
+        private System.Windows.Forms.Button button_sorting;
+        private System.Windows.Forms.TextBox textBox_sortingresult;
+
+    }
+}
+
---
+++ cpts322hw12 threading/Form1.cs
@@ -0,0 +1,248 @@
+﻿/*
+ * Siyang Li 11443579
+ * threading HW CptS322
+ * 
+ * Key idea, making threads under allsorting thread. So all while loops or Join() would not let the
+ *      UI thread freeze
+ * 
+ * 
+ * */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace cpts322hw12_threading
+{
+    public partial class Form1 : Form
+    {
+        WebClient wc = new WebClient();
+
+        public delegate void delUpdateUITextBox(string text);
+
+        ThreadStart threadstart_download;
+        Thread thread_download; //thread_download = new Thread(the parallel function I call);
+
+        string downloadedString = "";
+
+
+        // sorting part1
+        ThreadStart allEightThreadStart;// this is for single-threaded sorting
+        Thread allEightThread;
+        Stopwatch swsingle = new Stopwatch();// this StopWatch is for single-threaded sorting
+
+        //part2
+
+        List<Thread> tl = new List<Thread>(); // this is for multi-threaded sorting, going to add 8 thread to the list
+        private static Counter counter = new Counter();
+        Stopwatch swmulti = new Stopwatch();
+
+        Random rand = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void download_Click(object sender, EventArgs e)
+        {
+            if (urltextbox.Text != "")
+            {
+                // disable the button while task downloading is executing
+                urltextbox.Enabled = false;
+                download.Enabled = false;
+
+                threadstart_download = new ThreadStart(doDownload);
+                thread_download = new Thread(threadstart_download);
+                thread_download.Start();
+            }
+
+        }
+        public void doDownload()
+        {   
+
+            delUpdateUITextBox delUpdateUITextBox1 = new delUpdateUITextBox(UpdateUITextBox);
+
+            Uri stringurl = new Uri(urltextbox.Text);
+            downloadedString = wc.DownloadString(stringurl);
+
+            // invoke , call the UI thread method
+            this.downloadresultbox.BeginInvoke(delUpdateUITextBox1, downloadedString);
+        }
+
+        public void UpdateUITextBox(string newText)
+        {
+            this.downloadresultbox.Text = newText;
+            // enable download buttons and textbox after finished downloading
+            urltextbox.Enabled = true;
+            download.Enabled = true;
+        }
+
+        private void downloadresultbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // remember to add form close thread abort
+        ////////////////////////        sorting     ///////////////////////
+        Thread AllSorting;
+        private void button_sorting_Click(object sender, EventArgs e)
+        {
+            // samething, disable the button while executing
+            button_sorting.Enabled = false;
+
+            // this is the general thread that would do both single and multi threaded sorting
+            // I have single and multi (total 2) threads initialize in this thread
+            // and there would be more threads below...
+            AllSorting = new Thread(new ThreadStart(doAllSorting));
+            AllSorting.Start();
+
+            
+        }
+
+        public void doAllSorting()
+        {
+            counter.Count++;
+            Thread singlethread = new Thread(new ThreadStart(Sorting_SingleThread));
+            singlethread.Start();
+
+            //singlethread.Join();  // how to use join? here i use counter.Count to make sure single thread finished.
+            //idea is: before starting single-threaded sorting, count+1.
+            // at the moment it finish and get the result textbox updated, count-1
+            // so this line of code just keep track of counter.Count, see when it reaches 0. it represents single thread completed.
+            while (true) { if (counter.Count == 0) break; }
+
+
+            Thread multithread = new Thread(new ThreadStart(Sorting_MultiThread));
+            multithread.Start();
+            //Sorting_MultiThread();
+
+        }
+
+
+        public void Sorting_SingleThread()
+        {
+            allEightThreadStart = new ThreadStart(doSingleThread);
+            allEightThread = new Thread(allEightThreadStart);
+            allEightThread.Start();
+        }
+        public void doSingleThread()
+        {
+            delUpdateUITextBox delUpdateUITextBox2 = new delUpdateUITextBox(UpdateSortingTextBox2);
+            List<int> nums = new List<int>();
+            swsingle.Start();
+            for(int i = 0; i < 8; i++)
+            {
+                nums = fillList(); // filllist always generate a new random list and return to the list
+                nums.Sort();// make another thread only for this. 1 thread for ALL sorting
+            }
+            swsingle.Stop();
+            TimeSpan ts = swsingle.Elapsed;
+            string timeString = Convert.ToInt32(ts.TotalMilliseconds).ToString();
+            swsingle.Reset(); // reset the timer
+
+            // invoke call, update result textbox
+            this.textBox_sortingresult.BeginInvoke(delUpdateUITextBox2, timeString);
+            counter.Count--;// ==0.  Count - 1 to indicate this part is done, move on to multi-thread sorting
+        }
+
+        public void Sorting_MultiThread()
+        {
+
+            delUpdateUITextBox delUpdateUITextBox3 = new delUpdateUITextBox(UpdateSortingTextBox3);
+            for (int i = 0; i < 8; i++)// fill in the thread list with 8 threads
+            {
+                ThreadStart tstart = new ThreadStart(doMultiThread);
+                Thread t = new Thread(tstart);
+                tl.Add(t); //tl stands for thread list, which initialized on top/
+            }
+            swmulti.Start();
+            foreach (Thread t in tl) // start all threads in thread list
+            {
+                counter.Count++;
+                t.Start();
+            }
+            // same idea, use Count to keep track of the process 
+            while (true) { if (counter.Count == 0) break; }
+            swmulti.Stop();
+            TimeSpan ts = swmulti.Elapsed;
+            string timeString = Convert.ToInt32(ts.TotalMilliseconds).ToString();
+            swmulti.Reset();
+            this.textBox_sortingresult.BeginInvoke(delUpdateUITextBox3, timeString);
+            //textBox_sortingresult.Text += "multi thread time: " + timeString + "ms.\r\n";
+
+            tl.Clear(); // remember to clear thread list. 
+        }
+
+        public void doMultiThread()
+        {
+            // for each thread, generate random numbers and sort it.
+            List<int> nums = new List<int>();
+            nums = fillList();
+            nums.Sort();
+            counter.Count--;
+        }
+
+
+
+
+        public void UpdateSortingTextBox2(string newText)//for part1, update result of single-threaded sorting
+        {
+            this.textBox_sortingresult.Text += "Single-thread time: " + newText + "ms.\r\n"; // \r\n go to next line of textbox
+        }
+        public void UpdateSortingTextBox3(string newText)//for part2, update result of multi-threaded sorting
+        {
+            this.textBox_sortingresult.Text += "Multi-thread time: " + newText + "ms.\r\n"; // \r\n go to next line of textbox
+            button_sorting.Enabled = true;
+        }
+
+
+        // the counter class is made due to the idea of keep tracking if all threads are completed
+        public class Counter
+        {
+            private int count;
+            public int Count
+            {
+                get
+                {
+                    return count;
+                }
+                set
+                {
+                    count = value;
+                }
+            }
+            public Counter() { this.count = 0; }// default counter.Count == 0;
+        }
+
+        // this is the fill list method that generate and return to a int list (with 1000000 int in it)
+        private List<int> fillList()
+        {
+            List<int> l1 = new List<int>();
+            int num = 0;
+            for (int i = 0; i < 1000000; i++)
+            {
+                num = rand.Next(101);
+                l1.Add(num);
+            }
+            return l1;
+        }
+    }
+}
